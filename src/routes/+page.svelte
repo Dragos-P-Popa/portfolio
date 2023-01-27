5a7e5bcd8708a157052ef925a1f399b1757e05ac
Dragos Popa <pdragos66@gmail.com>
2023-01-27 23:31:59 +0000

    responsive changes for hero section, headings and 'other section'

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,8 +15,8 @@
 
 <div class="sticky z-50 drop-shadow-lg top-0 bg-gray-900 pt-3 pb-3 mb-[10%]">
     <div class="mx-[15%] flex flex-row">
-        <div class="basis-1/2 justify-self-start items-center">
-            <div class="max-w-fit rounded-md p-2 drop-shadow-lg" style="background-color: #374151;">
+        <div class="basis-0 md:basis-1/2 justify-self-start items-center">
+            <div class="collapse md:visible max-w-fit rounded-md p-2 drop-shadow-lg" style="background-color: #374151;">
                 <img alt="" class="pl-1" width="40" height="40" class:active on:mouseenter={() => {
                     src = "wave-animated.gif";
                     setTimeout(
@@ -29,33 +29,33 @@
                 }} id="wave" src={src}/>
             </div>
         </div>
-        <div class="basis-1/2 grid grid-cols-3 justify-self-end justify-items-center items-center">
+        <div class="md:basis-1/2 grid grid-cols-3 justify-self-end justify-items-center items-center">
             <!-- svelte-ignore a11y-click-events-have-key-events -->
             <div on:click={() => animateScroll.scrollToTop()} class="hover:drop-shadow-lg hover:scale-110 hover:border-solid hover:border-2 hover:border-emerald-600 border-solid border-2 border-transparent transition duration-150 ease-in-out delay-100 rounded-md p-2 bg-inherit">
                 <!-- svelte-ignore a11y-invalid-attribute -->
                 <a href="" >.home( )</a>
             </div>
             <!-- svelte-ignore a11y-click-events-have-key-events -->
-            <div on:click={() => animateScroll.scrollTo({element: '#about', offset: -100})} class="hover:drop-shadow-lg hover:scale-110 hover:border-solid hover:border-2 hover:border-emerald-600 border-solid border-2 border-transparent transition duration-150 ease-in-out delay-100 rounded-md p-2 bg-inherit">
-                <!-- svelte-ignore a11y-invalid-attribute -->
-                <a href="">.about( )</a>
-            </div>
-            <!-- svelte-ignore a11y-click-events-have-key-events -->
             <div on:click={() => animateScroll.scrollTo({element: '#projects', offset: -100})} class="hover:drop-shadow-lg hover:scale-110 hover:border-solid hover:border-2 hover:border-emerald-600 border-solid border-2 border-transparent transition duration-150 ease-in-out delay-100 rounded-md p-2 bg-inherit">
                 <!-- svelte-ignore a11y-invalid-attribute -->
                 <a href="">.projects( )</a>
+            </div>
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <div on:click={() => animateScroll.scrollTo({element: '#about', offset: -100})} class="hover:drop-shadow-lg hover:scale-110 hover:border-solid hover:border-2 hover:border-emerald-600 border-solid border-2 border-transparent transition duration-150 ease-in-out delay-100 rounded-md p-2 bg-inherit">
+                <!-- svelte-ignore a11y-invalid-attribute -->
+                <a href="">.about( )</a>
             </div>
         </div>	
     </div>
 </div>
 
 <div class="px-[15%]">
-    <h1 class="lg:text-5xl sm:text-xl font-bold subpixel-antialiased">
+    <h1 class="text-2xl md:text-4xl lg:text-5xl font-bold subpixel-antialiased">
         hey, i'm Dragos!
     </h1>
     <svg viewBox="0 0.09 5 0.25" xmlns="http://www.w3.org/2000/svg">
         {#if ready}
-            <path transition:draw="{{duration: 2000, easing: quintOut}}"
+            <path transition:draw="{{delay:500, duration: 2000, easing: quintOut}}"
             d="M 0,0.21 L 1.55,0.11 L 1.4,0.2 L 3,0.1"
             fill="none"
             stroke-linecap="round" 
@@ -64,83 +64,31 @@
             stroke-width="0.0175px" />
         {/if}
     </svg>
-    <p class="lg:text-3xl sm:text-sm font-light">
+    <p class="md:text-3xl font-light">
         an aspiring software engineer with a keen eye for design.
     </p>
 
     <div class="pt-10 flex items-center justify-start">
-        <div>
-            <img alt="" src="book-square.svg" width="40" height="40" />
+        <div class="flex-none">
+            <img class="w-auto" alt="" src="book-square.svg"  />
         </div>
         <div>
-            <p class="pl-4">MEng, BSc Computer Science @ University of Leeds</p>
+            <p class="text-xs md:text-xl pl-4">MEng, BSc Computer Science @ University of Leeds</p>
         </div>
     </div>
 
     <div class="pt-2 flex items-center justify-start">
-        <div>
-            <img alt="" src="direct-inbox.svg" width="40" height="40" />
+        <div class="flex-none">
+            <img class="w-auto" alt="" src="direct-inbox.svg"  />
         </div>
         <div>
-            <p class="pl-4">ed20dpp @ leeds.ac.uk</p>
+            <p class="text-xs md:text-xl pl-4">ed20dpp @ leeds.ac.uk</p>
         </div>
     </div>
 
 
-    <h1 id="about" class="lg:text-4xl sm:text-xl font-bold mt-[15%] pb-10 ">
-        .about( )
-    </h1>
-
-    <div class="grid grid-cols-4 gap-2">
-        <div class="grid grid-cols-4 justify-items-start items-center col-span-3">
-            <div class="col-span-1">
-                <img class="rounded-full hover:border-solid hover:border-2 hover:border-emerald-600 border-solid border-2 border-transparent transition duration-300 ease-in-out delay-150 " src="dragos.jpeg" alt="">
-            </div>
-            <div class="ml-10 col-span-3">
-                <h1 class="lg:text-3xl sm:text-xl font-bold mt-[10%] pb-2">
-                    Dragos Popa
-                </h1>
-                <p class="lg:text-md pb-10">
-                    2nd year Computer Science student
-                </p>                    
-            </div>
-            <div class="row-span-3 col-span-4 pt-6 whitespace-pre-line">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non ipsum tellus. Sed ac erat elit. Pellentesque aliquam, lectus sit amet molestie imperdiet, eros elit vulputate quam, vel sodales urna est quis ligula. Phasellus facilisis ullamcorper blandit. Donec pharetra, ex in auctor laoreet, erat ex tempus quam, et hendrerit quam turpis eget nulla. Proin posuere tincidunt semper. Vestibulum magna erat, malesuada non orci in, suscipit euismod quam. Sed venenatis eleifend eros, nec volutpat dolor ultrices ac. Sed id felis a diam sagittis fermentum in sed ante. Curabitur sollicitudin, ante non ultricies varius, nulla orci aliquet metus, id dignissim neque tortor in tellus. Nullam ut ullamcorper enim, quis pellentesque mauris. Praesent eu orci pretium, sollicitudin diam ac, feugiat sem. Aenean turpis ligula, mattis vel nunc sit amet, gravida tristique orci.
-            </div>
-        </div>
-        <div class="col-span-1 items-center">
-            <div class="border border-2 border-emerald-800 p-5 rounded-md">
-                <div class="flex items-center justify-start">
-                    <div>
-                        <img alt="" src="github.svg" width="40" height="40" />
-                    </div>
-                    <div>
-                        <a href="https://github.com/Dragos-P-Popa" target="_blank" rel="noreferrer" class="hover:underline pl-4">Dragos-P-Popa</a>
-                    </div>
-                </div>
-                
-                <div class="pt-2 flex items-center justify-start">
-                    <div>
-                        <img alt="" src="linkedin.svg" width="40" height="40" />
-                    </div>
-                    <div>
-                        <a href="https://www.linkedin.com/in/dragosppopa/" target="_blank" rel="noreferrer" class="hover:underline pl-4">Dragos Popa</a>
-                    </div>
-                </div>
-
-                <div class="pt-2 flex items-center justify-start">
-                    <div>
-                        <img alt="" src="resume.svg" width="40" height="40" />
-                    </div>
-                    <div>
-                        <a href="" target="_blank" rel="noreferrer" class="hover:underline pl-4">Resume</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
     
-    <h1 id="projects" class="lg:text-4xl sm:text-xl font-bold mt-[10%] pb-10">
+    <h1 id="projects" class="text-xl md:text-3xl lg:text-4xl font-bold mt-[15%] pb-10">
         .projects( )
     </h1>
 
@@ -191,7 +139,7 @@
     </div>
 
     
-    <h1 id="other" class="lg:text-3xl sm:text-md font-bold pt-16 pb-10 text-center min-w-[100%]">
+    <h1 id="other" class="text-xl md:text-2xl font-bold pt-16 pb-10 text-center min-w-[100%]">
         other noteworthy projects
     </h1>
 
@@ -200,23 +148,80 @@
         
         <a href="#" class="min-w-[100%] flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 transition transform hover:-translate-y-1 hover:-translate-x-1 motion-reduce:transition-none motion-reduce:hover:transform-none">
             <div class="flex flex-col justify-between p-4 leading-normal">
-                <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <h5 class="mb-2 text-md font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                <p class="mb-3 text-sm font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
             </div>
         </a>
 
         <a href="#" class="min-w-[100%] flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 transition transform hover:-translate-y-1 hover:-translate-x-1 motion-reduce:transition-none motion-reduce:hover:transform-none">
             <div class="flex flex-col justify-between p-4 leading-normal">
-                <h5 class="mb-1 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                <p class="mb-2 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <h5 class="mb-1 text-md lg:text-xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                <p class="mb-2 text-sm lg:text-md  font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
             </div>
         </a>
 
     </div>
 
-    
+    <h1 id="about" class="text-xl md:text-3xl lg:text-4xl font-bold mt-[10%] pb-10 ">
+        .about( )
+    </h1>
+
+    <div class="grid grid-cols-4 gap-2 mb-[10%]">
+        <div class="grid grid-cols-4 justify-items-start items-center col-span-3">
+            <div class="col-span-1">
+                <img class="rounded-full hover:border-solid hover:border-2 hover:border-emerald-600 border-solid border-2 border-transparent transition duration-300 ease-in-out delay-150 " src="dragos.jpeg" alt="">
+            </div>
+            <div class="ml-10 col-span-3">
+                <h1 class="lg:text-3xl sm:text-xl font-bold mt-[10%] pb-2">
+                    Dragos Popa
+                </h1>
+                <p class="lg:text-md pb-10">
+                    2nd year Computer Science student
+                </p>                    
+            </div>
+            <div class="row-span-3 col-span-4 pt-6 whitespace-pre-line">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non ipsum tellus. Sed ac erat elit. Pellentesque aliquam, lectus sit amet molestie imperdiet, eros elit vulputate quam, vel sodales urna est quis ligula. Phasellus facilisis ullamcorper blandit. Donec pharetra, ex in auctor laoreet, erat ex tempus quam, et hendrerit quam turpis eget nulla. Proin posuere tincidunt semper. Vestibulum magna erat, malesuada non orci in, suscipit euismod quam. Sed venenatis eleifend eros, nec volutpat dolor ultrices ac. Sed id felis a diam sagittis fermentum in sed ante. Curabitur sollicitudin, ante non ultricies varius, nulla orci aliquet metus, id dignissim neque tortor in tellus. Nullam ut ullamcorper enim, quis pellentesque mauris. Praesent eu orci pretium, sollicitudin diam ac, feugiat sem. Aenean turpis ligula, mattis vel nunc sit amet, gravida tristique orci.
+            </div>
+        </div>
+        <div class="col-span-1 items-center">
+            <div class="border border-2 border-emerald-800 p-5 rounded-md">
+                <div class="flex items-center justify-start">
+                    <div>
+                        <img alt="" src="github.svg" width="40" height="40" />
+                    </div>
+                    <div>
+                        <a href="https://github.com/Dragos-P-Popa" target="_blank" rel="noreferrer" class="hover:underline pl-4">Dragos-P-Popa</a>
+                    </div>
+                </div>
+                
+                <div class="pt-2 flex items-center justify-start">
+                    <div>
+                        <img alt="" src="linkedin.svg" width="40" height="40" />
+                    </div>
+                    <div>
+                        <a href="https://www.linkedin.com/in/dragosppopa/" target="_blank" rel="noreferrer" class="hover:underline pl-4">Dragos Popa</a>
+                    </div>
+                </div>
+
+                <div class="pt-2 flex items-center justify-start">
+                    <div>
+                        <img alt="" src="resume.svg" width="40" height="40" />
+                    </div>
+                    <div>
+                        <a href="" target="_blank" rel="noreferrer" class="hover:underline pl-4">Resume</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
+<hr class="h-px mt-6 bg-gray-200 border-0 dark:bg-gray-700">
+<footer>
+    <div class="grid items-center justify-items-center">
+        <p class="my-6">Made with &#10084; by Dragos Popa</p>
+    </div>
+</footer>
 
 <style lang="postcss">
 :global(html) {
